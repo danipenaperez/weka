0e5b3c759e7aa64fa6b031be60158361eebe4c12
Diego Palomo <diegopalomo@gmail.com>
2019-03-25 12:08:54 +0100

    Remove Sonar issues

--- src/main/java/com/dppware/wekaExamplesApplication/service/PredicctionService.java
+++ src/main/java/com/dppware/wekaExamplesApplication/service/PredicctionService.java
@@ -3,16 +3,14 @@ package com.dppware.wekaExamplesApplication.service;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.logging.Level;
 
 import javax.annotation.PostConstruct;
 
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
-import com.dppware.wekaExamplesApplication.bean.MatchInformation;
 import com.dppware.wekaExamplesApplication.bean.Person;
 
+import lombok.extern.slf4j.Slf4j;
 import weka.classifiers.Classifier;
 import weka.classifiers.evaluation.Evaluation;
 import weka.classifiers.functions.MultilayerPerceptron;
@@ -25,121 +23,112 @@ import weka.core.converters.ArffSaver;
 import weka.core.converters.ConverterUtils.DataSource;
 
 @Service
+@Slf4j
 public class PredicctionService {
 
-	
-	File f = new File("./data/person_attrs_new.arff");
-	
-	private DataSource source;
-	private Instances dataSet;
-	
-	
-	@PostConstruct
-	private void init() throws Exception {
-		this.source = new DataSource(new FileInputStream(f));
-		this.dataSet = source.getDataSet();
-		// setting class attribute if the data format does not provide this information
-		// For example, the XRFF format saves the class attribute information as well
-		// establece cual es el class (el target a conseguir) generalmente el ultimo
-		if (dataSet.classIndex() == -1) {
-			dataSet.setClassIndex(dataSet.numAttributes() - 1);
-		}
-	}
-	
-	/**
-	 * Store into current DataSet the option passed
-	 * @param person
-	 * @throws Exception
-	 */
-	public void learn(Person person) throws Exception {
-		//Create instance to Test
-		Instance ins = new DenseInstance(5);
-		ins.setDataset(dataSet);
-		ins.setValue(0, person.getGenre());
-		ins.setValue(1, person.getHairColour());
-		ins.setValue(2, person.getIsSpanish().toString());
-		ins.setValue(3, person.getProfession());
-		ins.setValue(4, person.getChoosed().toString());	
-		dataSet.add(ins);
-		
-		//log
-		System.out.println("current learned data " +this.dataSet.size());
-	}
-	
-	/**
-	 * TODO muy tipado
-	 * @param person
-	 * @return
-	 * @throws Exception
-	 */
-	public Boolean predice(Person person) throws Exception {
-		// Prepare the filter
-		J48 tree = new J48();
-		tree.setUnpruned(true);
-		tree.buildClassifier(dataSet);
-		
-		//Create instance to Test
-		Instance ins = new DenseInstance(4);
-		ins.setDataset(dataSet);
-		ins.setValue(0, person.getGenre());
-		ins.setValue(1, person.getHairColour());
-		ins.setValue(2, person.getIsSpanish().toString());
-		ins.setValue(3, person.getProfession());
-		
-		int result = (int) tree.classifyInstance(ins);
-		
-		return result==1;
-	}
-	
-	
-	/**
-	 * Persist current DataSet to file
-	 * @throws IOException
-	 */
-	private void persistCurrentDataSet() throws IOException {
-		ArffSaver saver = new ArffSaver();
-		saver.setInstances(dataSet);
-		saver.setFile(f);
-		saver.writeBatch();
-	}
-	
-	
-	
-	
-	
-	public Classifier buildClassifier(Instances traindataset) {
+    private File f = new File("./data/person_attrs_new.arff");
+    private Instances dataSet;
+
+    @PostConstruct
+    private void init() throws Exception {
+        DataSource source = new DataSource(new FileInputStream(f));
+        this.dataSet = source.getDataSet();
+        // setting class attribute if the data format does not provide this information
+        // For example, the XRFF format saves the class attribute information as well
+        // establece cual es el class (el target a conseguir) generalmente el ultimo
+        if (dataSet.classIndex() == -1) {
+            dataSet.setClassIndex(dataSet.numAttributes() - 1);
+        }
+    }
+
+    /**
+     * Store into current DataSet the option passed
+     * 
+     * @param person
+     * @throws Exception
+     */
+    public void learn(Person person) throws Exception {
+        // Create instance to Test
+        Instance ins = new DenseInstance(5);
+        ins.setDataset(dataSet);
+        ins.setValue(0, person.getGenre());
+        ins.setValue(1, person.getHairColour());
+        ins.setValue(2, person.getIsSpanish().toString());
+        ins.setValue(3, person.getProfession());
+        ins.setValue(4, person.getChoosed().toString());
+        dataSet.add(ins);
+
+        // log
+        log.info("current learned data " + this.dataSet.size());
+    }
+
+    /**
+     * TODO muy tipado
+     * 
+     * @param person
+     * @return
+     * @throws Exception
+     */
+    public Boolean predice(Person person) throws Exception {
+        // Prepare the filter
+        J48 tree = new J48();
+        tree.setUnpruned(true);
+        tree.buildClassifier(dataSet);
+
+        // Create instance to Test
+        Instance ins = new DenseInstance(4);
+        ins.setDataset(dataSet);
+        ins.setValue(0, person.getGenre());
+        ins.setValue(1, person.getHairColour());
+        ins.setValue(2, person.getIsSpanish().toString());
+        ins.setValue(3, person.getProfession());
+
+        int result = (int) tree.classifyInstance(ins);
+
+        return result == 1;
+    }
+
+    /**
+     * Persist current DataSet to file
+     * 
+     * @throws IOException
+     */
+    private void persistCurrentDataSet() throws IOException {
+        ArffSaver saver = new ArffSaver();
+        saver.setInstances(dataSet);
+        saver.setFile(f);
+        saver.writeBatch();
+    }
+
+    public Classifier buildClassifier(Instances traindataset) {
         MultilayerPerceptron m = new MultilayerPerceptron();
         try {
             m.buildClassifier(traindataset);
-
         } catch (Exception ex) {
-            System.out.println(ex.toString());
+            log.error(ex.toString());
         }
         return m;
     }
-	
-	public String evaluateModel(Classifier model, Instances traindataset, Instances testdataset) {
+
+    public String evaluateModel(Classifier model, Instances traindataset, Instances testdataset) {
         Evaluation eval = null;
         try {
             // Evaluate classifier with test dataset
             eval = new Evaluation(traindataset);
             eval.evaluateModel(model, testdataset);
         } catch (Exception ex) {
-        	System.out.println(ex.toString());
+            log.error(ex.toString());
         }
         return eval.toSummaryString("", true);
     }
-	
-	
-	public void saveModel(Classifier model, String modelpath) {
+
+    public void saveModel(Classifier model, String modelpath) {
 
         try {
             SerializationHelper.write(modelpath, model);
         } catch (Exception ex) {
-        	System.out.println(ex.toString());
+            log.error(ex.toString());
         }
     }
-	
-	
-	
+
 }
